27b7be25a6411b948c9f2b985a4b0752a697ba40
Yoshi Ishigami <yoshi@ibf.sc>
2015-06-20 13:06:04 +0900

    made update

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,14 @@
 class Customer < ActiveRecord::Base
+  validates :family_name,
+    presence: true, length: { maximum: 20 } #バリデーションの条件 
+  validates :given_name,
+    presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i#宣言
+  validates :email,  
+    presence: true, format: { with: VALID_EMAIL_REGEX }
+
   def full_name #変数名
     full_name = family_name + given_name
-    full_name #
+    full_name
   end
 end
